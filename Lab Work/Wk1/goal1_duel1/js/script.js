bb4fb5c221cdd3c44d790a436b966a8cc9340040
Russ93 <russ93@fullsail.edu>
2013-09-04 14:52:34 -0400

    Lab1 Switch from an "IF" to a "while loop"

--- Lab Work/Wk1/goal1_duel1/js/script.js
+++ Lab Work/Wk1/goal1_duel1/js/script.js
@@ -1,5 +1,5 @@
-var p1Name = "Xandir Cruiz";
-var p2Name = "Bob Thatcher";
+var p1Name = "Xandir Cruiz: ";
+var p2Name = "Bob Thatcher: ";
 
 var p1Health = 100;
 var p2Health = 100;
@@ -8,10 +8,22 @@ var p1Dam = 20;
 var p2Dam = 25;
 
 var round = 0;
+var rText = "";
 
-/* if(p1Health>=0&&p2Health>=0){ */
-	p1Health=-p2Dam;
-	p2Health=-p1Dam;
-	console.log("Xandir has "+p1Health+" health left.");
-	console.log("Bob has "+p2Health+" health left.");
-/* } */
+while(p1Health>0&&p2Health>0){
+	if(round=0){
+		rText="Start";
+	}else{
+		rText="Round "+round;
+	}
+/* 	alert(p1Name+p1Health+" "+rText+" "+p2Name+p2Health); */
+	if(p1Health>0){
+		p1Health=p1Health-p2Dam;
+	}else if(p2Health>0){
+		p2Health=p2Health-p1Dam;
+	}
+	round++;
+	console.log(p1Health);
+	console.log(p2Health);
+	console.log(round);
+}
