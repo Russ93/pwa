46cfe537ce64bd29328ee6f47baf6af82ccdbce3
Russ93 <russ93@fullsail.edu>
2013-09-04 15:14:36 -0400

    Lab1 StartOver

--- Lab Work/Wk1/goal1_duel1/js/script.js
+++ Lab Work/Wk1/goal1_duel1/js/script.js
@@ -1,29 +1,24 @@
+
+console.log("FIGHT!!!");
+
 var p1Name = "Xandir Cruiz: ";
 var p2Name = "Bob Thatcher: ";
 
 var p1Health = 100;
 var p2Health = 100;
 
-var p1Dam = 20;
-var p2Dam = 25;
+var p1Dam = 25; /* Math.random()*(25) */
+var p2Dam = 20; /* Math.random()*(20) */
 
-var round = 0;
-var rText = "";
+var rnd = 0;
 
-while(p1Health>0&&p2Health>0){
-	if(round=0){
-		rText="Start";
-	}else{
-		rText="Round "+round;
-	}
-/* 	alert(p1Name+p1Health+" "+rText+" "+p2Name+p2Health); */
-	if(p1Health>0){
-		p1Health=p1Health-p2Dam;
-	}else if(p2Health>0){
-		p2Health=p2Health-p1Dam;
-	}
-	round++;
-	console.log(p1Health);
-	console.log(p2Health);
-	console.log(round);
-}
+function fight(){
+	winnerCheck()
+	
+};
+
+function winnerCheck(){
+	
+};
+
+fight();
