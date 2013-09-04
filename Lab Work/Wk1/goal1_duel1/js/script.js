5ea9403bfe819fddd2c177b4506ce70cf84aa21c
Russ93 <russ93@fullsail.edu>
2013-09-04 14:24:26 -0400

    Initialzed Lab1

---
+++ Lab Work/Wk1/goal1_duel1/js/script.js
@@ -0,0 +1,17 @@
+var p1Name = "Xandir Cruiz";
+var p2Name = "Bob Thatcher";
+
+var p1Health = 100;
+var p2Health = 100;
+
+var p1Dam = 20;
+var p2Dam = 25;
+
+var round = 0;
+
+/* if(p1Health>=0&&p2Health>=0){ */
+	p1Health=-p2Dam;
+	p2Health=-p1Dam;
+	console.log("Xandir has "+p1Health+" health left.");
+	console.log("Bob has "+p2Health+" health left.");
+/* } */
