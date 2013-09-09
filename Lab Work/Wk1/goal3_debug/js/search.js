4fe51bcbe6035858e176235e8f17696cdd9da2e2
Russ93 <russ93@fullsail.edu>
2013-09-09 12:08:31 -0400

    Js Linted

--- Lab Work/Wk1/goal3_debug/js/search.js
+++ Lab Work/Wk1/goal3_debug/js/search.js
@@ -1,39 +1,19 @@
 // 16 total errors
 (function () {
-	// Variable initialization (DO NOT FIX ANY OF THE BELOW VAR's)
-	var resultsDIV = document.getElementById("results"),
-		searchInput = document.forms[0].search,
+	"use strict";
+    // Variable initialization (DO NOT FIX ANY OF THE BELOW VAR's)
+    var resultsDIV = document.getElementById("results"),
+        searchInput = document.forms[0].search,
 		currentSearch = '';
 	
-	// Validates search query
-	var validqte = function(query){
-		// Trim whitespace from start and end of search query
-		while(query.charAt(0) == " "){
-			query = query.substring(1, query.length);
-		};
-		while(query.charAt(query.length-1) === " "){
-			query = query.substring(0, query.length-1);
-		};
-		
-		// Check search length, must have 3 characters
-		if(query.length < 3){
-			alert("Your search query is too small, try again.");
-			
-			// (DO NOT FIX THE LINE DIRECTLY BELOW)
-			searchInput.focus();
-			return;
-		};
-		goSearch(query);
-	};
-	
-	// Finds search matches
-	var goSearch = function(query){
+		// Finds search matches
+	function searchSomething(query){
 		
 		// split the user's search query string into an array
 		var queryArray = query.split(" ");
-		
+
 		// array to store matched results from database.js
-		var results = [];
+		results = [];
 	
 			// loop through each index of db array
 		for(var i=0, j=db.length; i<j; i++){
@@ -46,16 +26,16 @@
 			// loop through the user's search query words
 			// save a lowercase variable of the search keyword
 			for(var ii=0, jj=queryArray.length; ii<jj; ii++){
-				var qitem = queryArray[ii];
+				var qitem = queryArray[ii]
 				
 				// is the keyword anywhere in the video title?
 				// If a match is found, push full db[i] into results array
-				var compare = dbitem.indexOf(qitem);
+				compare = dbitem.indexOf(qitem);
 				if(compare !== -1){
 					results.push(db[i]);
-				};
-			};
-		};
+				}
+			}
+		}
 			
 			results.sort();
 			
@@ -64,20 +44,39 @@
 				noMatch();
 			}else{
 				showMatches(results);
-			};//else
-	};//function
+			}//else
+	}//function
+
+	// Validates search query
+	function validqte(query){
+		// Trim whitespace from start and end of search query
+		while(query.charAt(0) === " "){
+			query = query.substring(1, query.length);
+		}
+		while(query.charAt(query.length-1) === " "){
+			query = query.substring(0, query.length-1);
+		}
+		// Check search length, must have 3 characters
+		if(query.length < 3){
+			alert("Your search query is too small, try again.");
+			// (DO NOT FIX THE LINE DIRECTLY BELOW)
+			searchInput.focus();
+			return;
+		}
+		searchSomething(query);
+	}
 	
 	// Put "No Results" message into page (DO NOT FIX THE HTML VAR NOR THE innerHTML)
-	var noMatch = function(){
+	function noMatch(){
 		var html = ''+
 			'<p>No Results found.</p>'+
 			'<p style="font-size:10px;">Try searching for "JavaScript".  Just an idea.</p>'
 		;
 		resultsDIV.innerHTML = html;
-	};
+	}
 	
 	// Put matches into page as paragraphs with anchors
-	var showMatches = function(results){
+	function showMatches(results){
 		
 		// THE NEXT 4 LINES ARE CORRECT.
 		var html = '<p>Results</p>', 
@@ -97,9 +96,9 @@
 			
 			// make the video link - THE NEXT LINE IS CORRECT.
 			html += '<p><a href=' + url + '>' + title + '</a></p>';
-		};
+		}
 		resultsDIV.innerHTML = html; //THIS LINE IS CORRECT.
-	};
+	}
 	
 	// The onsubmit event will be reviewed in upcoming Course Material.
 	// THE LINE DIRECTLY BELOW IS CORRECT
@@ -110,5 +109,5 @@
         // return false is needed for most events - this will be reviewed in upcoming course material
         // THE LINE DIRECTLY BELOW IS CORRECT
 		return false;
-		};
+		}
 })();
